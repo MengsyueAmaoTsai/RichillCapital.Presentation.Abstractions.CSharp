9dc392c0e945ec2e771cf0bee69fa0ff3f889e90
Mengsyue Amao Tsai <mengsyue.tsai@outlook.com>
2024-01-05 06:14:17 +0800

    Add HandleFailure to map ErrorType to ActionResult

--- RichillCapital.Presentation.Abstractions/AspNetCore/Endpoint.cs
+++ RichillCapital.Presentation.Abstractions/AspNetCore/Endpoint.cs
@@ -1,8 +1,23 @@
 using Microsoft.AspNetCore.Mvc;
 
+using RichillCapital.Extensions.Primitives;
+
 namespace RichillCapital.Presentation.Abstractions.AspNetCore;
 
 [ApiController]
 public abstract class EndpointBase : ControllerBase
 {
+    protected ActionResult HandleFailure(Error error)
+    {
+        return error.Type switch
+        {
+            ErrorType.Invalid => BadRequest(error.Message),
+            ErrorType.NotFound => NotFound(error.Message),
+            ErrorType.Conflict => BadRequest(error.Message),
+            ErrorType.Unauthorized => Unauthorized(error.Message),
+            ErrorType.Default => throw new NotImplementedException(),
+            ErrorType.Null => throw new NotImplementedException(),
+            _ => throw new NotImplementedException(),
+        };
+    }
 }
